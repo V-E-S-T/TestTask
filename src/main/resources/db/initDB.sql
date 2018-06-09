20284fa8f3955d007643a54c9295ceaf4cdf5ee6
Vasiliev Evgenij <vasiliev.evgenij@gmail.com>
2018-06-09 09:28:22 +0300

    Initial commit

---
+++ src/main/resources/db/initDB.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS users;
+DROP SEQUENCE IF EXISTS global_seq;
+
+CREATE SEQUENCE global_seq START 100000;
+
+CREATE TABLE users
+(
+  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  firstName            VARCHAR                 NOT NULL,
+  lastName             VARCHAR                 NOT NULL,
+  birthDay             TIMESTAMP               NOT NULL,
+  gender               VARCHAR                 NOT NULL
+);
